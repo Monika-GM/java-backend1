87ffb6020ac903efb3f1fe32857fa3aed89ba7d4
Monika-GM <monika.gm@collabera.com>
2022-01-04 16:47:44 +0530

    java recap : Car class and test cases for object and constructor

---
+++ week1/day2/src/test/java/clsobj/Car.java
@@ -0,0 +1,14 @@
+package clsobj;
+
+public class Car {
+    // state
+    public int speed;
+
+    //default
+    public Car() { speed = 10;  }
+
+    //parameterized
+    public Car(int sp) {
+        speed = sp;
+    }
+}
