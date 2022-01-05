82966018e2817bcead72208309c851ef702c6eee
Monika-GM <monika.gm@collabera.com>
2022-01-05 16:06:03 +0530

    java collections : set, list, map, queue operations

--- week1/day3/java-other-concepts/src/main/java/collections/CollectionDemo.java
+++ week1/day3/java-other-concepts/src/main/java/collections/CollectionDemo.java
@@ -5,7 +5,6 @@ import java.util.function.Consumer;
 
 public class CollectionDemo {
     public List<String> listDemo() {
-
         ArrayList<String> names = new ArrayList<>();
         names.add("abc");
         names.add("pqr");
@@ -14,6 +13,12 @@ public class CollectionDemo {
         names.add(null);
         names.add("xyz");
 
+//        names.clear(); it will delete all elements in list
+        names.add(4, "hji");
+        names.contains("abc");
+        names.remove(2);
+        names.remove("xyz");
+
         Consumer<String> cls = new Consumer<String>() { // anonymous inner class -> traditional approach
             @Override
             public void accept(String s) {
@@ -21,13 +26,10 @@ public class CollectionDemo {
             }
         };
         names.forEach(cls);
-
         Consumer<String> fn = (String s) -> System.out.println(s); // functional interface -> java8 and above approach
         names.forEach(fn);
-
         return names;
     }
-
     public Set<String> setDemo() {
         HashSet<String> set = new HashSet<>();
         set.add("abc");
@@ -37,11 +39,12 @@ public class CollectionDemo {
         set.add(null);
         set.add("xyz");
 
+        set.remove("xyz");
+
         set.forEach( (s) -> System.out.println(s) ); // lambda
 
         return set;
     }
-
     public Queue<String> queueDemo() {
         Queue<String> queue = new LinkedList<>();
         queue.add("abc");
@@ -50,15 +53,50 @@ public class CollectionDemo {
         queue.add(null);
         queue.add("pqr");
 
+        queue.offer("android");
+        queue.peek();
+        queue.poll();
+
+        // forEach is asking for a parameter
+        // as function who returns void and accepts one string parameter
         queue.forEach(System.out::println); // method reference
 
         return queue;
+    }
+
+    public void mapDemo() {
+
+        Map<Integer, String> map = new HashMap<Integer, String>();
+        map.put(1, "abc");
+        map.put(2, "pqr");
+        map.put(3, "lmn");
+        map.put(4, "xyz");
+
+        Set<Integer> keys =  map.keySet();
+        keys.forEach( key -> System.out.println("Key - "+key) );
+
+        Collection<String> values = map.values();
+        values.forEach( val -> System.out.println("Value - "+val) );
+
+        map.remove(1, "abc");
+        System.out.println("2 available -> "+map.containsKey(2));;
+        System.out.println("lmn available -> "+map.containsValue("lmn"));;
+        System.out.println("Element At 3 -> "+ map.getOrDefault(3, "none"));
+
+        map.forEach( (k, v) -> System.out.println("Key - "+k +" Value - "+v) );
     }
 
     public static void main(String[] args) {
         CollectionDemo cdm = new CollectionDemo();
 //        cdm.listDemo();
 //        cdm.setDemo();
+        System.out.println("----- List Demo -----");
+        cdm.listDemo();
+        System.out.println("----- Set Demo -----");
+        cdm.setDemo();
+        System.out.println("----- Queue Demo -----");
         cdm.queueDemo();
+        System.out.println("----- Map Demo -----");
+        cdm.mapDemo();
     }
 }
