e720e14cce1f931bca900a7e7d96138f1286f30f
Monika-GM <monika.gm@collabera.com>
2022-01-05 09:08:53 +0530

    task : learning inheritance along with unit testing Bmw2

---
+++ week1/day2/src/test/java/BmwTest1.java
@@ -0,0 +1,43 @@
+import clsobj.Bmw;
+import clsobj.Car;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BmwTest1 {
+    @DisplayName("Testing Object Nullability")
+    @Test
+    void testInValidObject() {
+        BmwTests bmw = null;
+        Assertions.assertNull(bmw);
+    }
+
+    @DisplayName("Testing Object Validity")
+    @Test
+    void testValidObject() {
+        Bmw bmw = new Bmw();
+
+        Assertions.assertNotNull(bmw);
+
+        int expectedSpeed = 10;
+        int actualSpeed = bmw.speed;
+        Assertions.assertEquals(expectedSpeed, actualSpeed);
+    }
+    @DisplayName("Test is Bmw is a Car")
+    @Test
+    void testBmwObjectCar() {
+        Bmw bmw = new Bmw();
+        boolean expected = true;
+        boolean actual = bmw instanceof Car;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @DisplayName("Test Car is Not a BMW")
+    @Test
+    void testCarObjectNotBmw() {
+        Car car = new Car();
+        boolean expected = false;
+        boolean actual = car instanceof Bmw;
+        Assertions.assertEquals(expected, actual);
+    }
+}
