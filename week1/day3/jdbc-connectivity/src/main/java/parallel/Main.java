671408c28c9a1bcd8186729ad993c95dc990de46
Monika-GM <monika.gm@collabera.com>
2022-01-06 18:00:21 +0530

    parallel programming : futures

--- week1/day3/jdbc-connectivity/src/main/java/parallel/Main.java
+++ week1/day3/jdbc-connectivity/src/main/java/parallel/Main.java
@@ -1,13 +1,18 @@
 package parallel;
 
+import java.lang.reflect.Executable;
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Main {
     public void withStream() {
         IntStream
-                .of(1,7,4,3,6,8)
+                .of(2,4,6,8)
                 .sequential()
                 .filter( el -> el > 10 )
 
@@ -18,20 +23,50 @@ public class Main {
 
     public void withoutStream() {
         ArrayList<Integer> nums = new ArrayList<>();
-        nums.add(1);
-        nums.add(7);
+        nums.add(2);
         nums.add(4);
-        nums.add(3);
         nums.add(6);
         nums.add(8);
+
 
         for (int num : nums ) {
             System.out.println(num * num);
         }
     }
+    public void futures() {
+        ExecutorService service = Executors.newFixedThreadPool(5);
+
+        Future<String> task1 = service.submit(() -> {
+            // uploading images to server
+            Thread.sleep(2500);
+            return "Hello World From Task 1";
+        });
+
+        Future<String> task2 = service.submit(() -> {
+            // extracting data from spreadsheet
+            Thread.sleep(5500);
+            return "Hello World From Task 2";
+        });
+
+        try {
+            if(!task1.isCancelled()) {
+                System.out.println( " Task1 Completed : "+ task1.get() );
+            }
+            if(!task2.isCancelled()) {
+                System.out.println( " Task2 Completed : "+ task2.get() );
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        service.shutdown();
+    }
 
     public static void main(String[] args) {
         Main main = new Main();
         main.withoutStream();
+        main.futures();
     }
+
+
 }
