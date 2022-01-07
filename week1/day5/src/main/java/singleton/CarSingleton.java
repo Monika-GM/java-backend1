5454752609ac6589daed12f8adb0705e1197d53d
Monika-GM <monika.gm@collabera.com>
2022-01-07 12:23:23 +0530

    design patterns : singleton

---
+++ week1/day5/src/main/java/singleton/CarSingleton.java
@@ -0,0 +1,13 @@
+package singleton;
+
+public class CarSingleton {
+    private static Car car;
+
+    private CarSingleton() {
+    }
+
+    public static Car newInstance() {
+        if(car == null) return car = new Car();
+        return car;
+    }
+}
