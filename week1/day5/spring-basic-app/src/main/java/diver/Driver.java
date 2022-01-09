005e2637b26066ea64c8219f47181c1c6cb9e68a
Monika-GM <monika.gm@collabera.com>
2022-01-09 20:03:15 +0530

    spring : ioc container and beans

---
+++ week1/day5/spring-basic-app/src/main/java/diver/Driver.java
@@ -0,0 +1,15 @@
+package diver;
+
+import car.Car;
+
+public class Driver {
+    private final Car car;
+
+    public Driver(Car car) {
+        this.car = car;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+}
---
+++ week1/day5/spring-basic-app/src/main/java/tempo/Tempo.java
@@ -0,0 +1,15 @@
+package tempo;
+
+import car.Driver;
+
+public class Tempo {
+    private Driver driver;
+
+    public Driver getDriver() {
+        return driver;
+    }
+
+    public void setDriver(Driver driver) {
+        this.driver = driver;
+    }
+}
