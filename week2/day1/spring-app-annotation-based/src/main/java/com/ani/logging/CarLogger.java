bf26dee0df2b410089512ef79dcbcd2e63308c4b
Monika-GM <monika.gm@collabera.com>
2022-01-10 14:55:50 +0530

    spring aop : point cuts

--- week2/day1/spring-app-annotation-based/src/main/java/com/ani/logging/CarLogger.java
+++ week2/day1/spring-app-annotation-based/src/main/java/com/ani/logging/CarLogger.java
@@ -1,8 +1,10 @@
 package com.ani.logging;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -11,6 +13,8 @@ import org.springframework.stereotype.Component;
 public class CarLogger {
     //execution(public String com.ani.car.Car.speedUp())
     //execution(* com.ani.car.*.*(..))
+    @Pointcut("execution(public * com.ani.car.Car.speedUp(..))")
+    public void pointCutAfterExecution() { }
 
     @Before("execution(* com.ani.car.*.*(..))")
     public void logBeforeAnyMethodExecutionCarPackage(JoinPoint jp) {
@@ -18,5 +22,9 @@ public class CarLogger {
         System.out.println("Executing method "+jp.getSignature());
         System.out.println("After this line you will see method output");
         System.out.println("----------------");
+    }
+    @After("pointCutAfterExecution()")
+    public void logAfterAnyMethods(JoinPoint jp) {
+        System.out.println("After Execution I am getting printed");
     }
 }
