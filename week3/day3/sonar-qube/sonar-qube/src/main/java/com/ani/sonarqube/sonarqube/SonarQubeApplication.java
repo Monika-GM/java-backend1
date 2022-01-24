2ebb3a84b76132a68c183c2641096caa825adece
Monika-GM <monika.gm@collabera.com>
2022-01-24 11:10:48 +0530

    ani practice

---
+++ week3/day3/sonar-qube/sonar-qube/src/main/java/com/ani/sonarqube/sonarqube/SonarQubeApplication.java
@@ -0,0 +1,13 @@
+package com.ani.sonarqube.sonarqube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarQubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarQubeApplication.class, args);
+	}
+
+}
